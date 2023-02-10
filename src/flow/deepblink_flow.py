418a492ce9184701235ce23fc877e1108f872ecc
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-02-10 10:02:56 +0100

    Refactor deepblink flow.

--- src/flow/deepblink_flow.py
+++ src/flow/deepblink_flow.py
@@ -1,42 +1,71 @@
+import threading
+from glob import glob
+from os.path import join
 from pathlib import Path
-from typing import Union, Literal, List, Dict
+from typing import List, Dict, Any, Optional
 import os
 
+import tensorflow as tf
 from cpr.Serializer import cpr_serializer
 from cpr.image.ImageSource import ImageSource
-from cpr.image.ImageTarget import ImageTarget
 from cpr.utilities.utilities import task_input_hash
 import deepblink as pink
-import koopa
 
-# import numpy as np
 import prefect
+from koopa.detect import detect_image
+from prefect import get_client
+from prefect.client.schemas import FlowRun
+from prefect.deployments import run_deployment
 
 from .cpr_parquet import ParquetTarget
 
+
+def exclude_sem_and_model_input_hash(
+    context: "TaskRunContext", arguments: Dict[str, Any]
+) -> Optional[str]:
+    hash_args = {}
+    for k, item in arguments.items():
+        if (not isinstance(item, threading.Semaphore)) and (
+            not isinstance(item, tf.keras.Model)
+        ):
+            hash_args[k] = item
+
+    return task_input_hash(context, hash_args)
 
 @prefect.task(
     name="deepblink-single-channel",
     cache_result_in_memory=False,
     persist_result=True,
-    cache_key_fn=task_input_hash,
+    cache_key_fn=exclude_sem_and_model_input_hash,
 )
 def deepblink_spot_detection_task(
-    image: ImageSource, detection_channel: int, deepblink_model: Path
+    image: ImageSource,
+        detection_channel: int,
+        out_dir: Path,
+        model: tf.keras.Model,
+        gpu_sem: threading.Semaphore
 ) -> ParquetTarget:
     logger = prefect.get_run_logger()
     logger.info("running task")
 
-    model = pink.io.load_model(deepblink_model)
-    df = koopa.detect.detect_image(
-        image.get_data(), detection_channel, model, refinement_radius=3
-    )
+    data = image.get_data()
+
+    try:
+        gpu_sem.acquire()
+        df = detect_image(
+            data, detection_channel, model, refinement_radius=3
+        )
+    except RuntimeError as e:
+        raise e
+    finally:
+        gpu_sem.release()
+
+
     df.insert(loc=0, column="FileID", value=image.get_name())
     logger.info("detected")
 
-    path_base = os.path.abspath(os.path.join(image.get_path(), os.pardir, os.pardir))
     fname_out = os.path.join(
-        path_base, f"test2d_detection_c{detection_channel}", f"{image.get_name()}.parq"
+        out_dir, f"test2d_detection_c{detection_channel}", f"{image.get_name()}.parq"
     )
     output = ParquetTarget.from_path(fname_out)
     output.set_data(df)
@@ -56,17 +85,32 @@ def deepblink_spot_detection_task(
     validate_parameters=False,
 )
 def deepblink_spot_detection_flow(
-    preprocessed: ImageSource,
+    serialized_preprocessed: List[dict],
+    out_dir: Path,
     detection_channels: List[int],
     deepblink_models: List[Path],
 ) -> List[Dict[int, ParquetTarget]]:
     logger = prefect.get_run_logger()
     logger.info("running sub flow")
 
-    for channel, model in zip(detection_channels, deepblink_models):
-        output = deepblink_spot_detection_task.submit(
-            preprocessed, channel, model
-        ).wait()
+    preprocessed = [ImageSource(**d) for d in serialized_preprocessed]
+
+    gpu_sem = threading.Semaphore(1)
+
+    output = {}
+    for channel, model_path in zip(detection_channels, deepblink_models):
+        model = pink.io.load_model(model_path)
+        detections = []
+        for img in preprocessed:
+            detections.append(deepblink_spot_detection_task.submit(
+                image=img,
+            detection_channel=channel,
+            out_dir=out_dir,
+            model=model,
+            gpu_sem=gpu_sem,
+            ))
+
+        output[channel] = detections
 
     logger.info(f"output from task - {output}")
     logger.info("finished sub flow")
@@ -82,21 +126,28 @@ def deepblink_spot_detection_flow(
 def run_deepblink(
     input_path: Path = "/path/to/acquisition/dir",
     output_path: Path = "/path/to/output/dir",
-    file_ext: str = "nd",
+    pattern: str = "*.tif",
     detection_channels: List[int] = [0],
     deepblink_models: List[Path] = ["/path/to/model.h5"],
 ):
-    logger = prefect.get_run_logger()
-    logger.info("running main flow")
+    images = [ImageSource.from_path(p) for p in glob(join(input_path,
+                                                          pattern))]
 
-    # TODO add nd file parsing from input_path
-    # TODO add preprocessing step saving in output_path
-    fname = "/tungstenfs/scratch/gchao/eichbast/koopa-data/input_2d/20221115_EGFP_R3_3h_16.tif"
-    deepblink_models = ["/tungstenfs/scratch/gchao/deepblink/model_fish.h5"]
+    images_dicts = [img.serialize() for img in images]
 
-    image = koopa.io.load_image(fname)
-    output = ImageTarget.from_path(fname)
-    output.set_data(image)
-    deepblink_spot_detection_flow(output, detection_channels, deepblink_models)
+    parameters = {
+        "serialized_preprocessed": images_dicts,
+        "out_dir": output_path,
+        "detection_channels": detection_channels,
+        "deepblink_models": deepblink_models,
+    }
 
-    logger.info("finished main flow")
+    run: FlowRun = run_deployment(
+        name="deepblink/default",
+        parameters=parameters,
+        client=get_client(),
+    )
+
+    detections = run.state.result()
+    return detections
+
